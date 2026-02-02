b85ded10d13c5cad046d9f2c2de372d80e28969a
aveenash kumar <aveenashkumar68@gmail.com>
2026-02-02 22:40:22 +0530

    Cors setting

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,21 @@ connectDB();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://note-app-gamma-two.vercel.app"
+];
+
 app.use(cors({
-    origin: "http://localhost:5173",
-  credentials: true,
-}
-));
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed"));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
